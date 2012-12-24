4ccbf307a615249f1d16d46b6351a537cdbbcdae
Thiyagu <thiyagu@bubblepath.com>
2012-12-24 17:49:09 +0530

    Invoice model changes

--- chargebee/models/invoice.py
+++ chargebee/models/invoice.py
@@ -11,16 +11,28 @@ class Invoice(Model):
 
 
     @staticmethod
+    def add_charge(id, params, env=None):
+        return request.send('post', '/invoices/%s/add_charge' % id, params, env)
+
+    @staticmethod
+    def add_addon_charge(id, params, env=None):
+        return request.send('post', '/invoices/%s/add_addon_charge' % id, params, env)
+
+    @staticmethod
     def list(params=None, env=None):
         return request.send('get', '/invoices', params, env)
+
+    @staticmethod
+    def invoices_for_subscription(id, params=None, env=None):
+        return request.send('get', '/subscriptions/%s/invoices' % id, params, env)
 
     @staticmethod
     def retrieve(id, env=None):
         return request.send('get', '/invoices/%s' % id, None, env)
 
     @staticmethod
-    def invoices_for_subscription(id, params=None, env=None):
-        return request.send('get', '/subscriptions/%s/invoices' % id, params, env)
+    def collect(id, env=None):
+        return request.send('post', '/invoices/%s/collect' % id, None, env)
 
     @staticmethod
     def charge(params, env=None):
