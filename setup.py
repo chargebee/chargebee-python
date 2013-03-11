4187e529c16b9fe82c3e84a37f2f023b33e30298
rraman <rr@chargebee.com>
2013-03-11 17:50:14 +0530

    version bump to 1.0.6
    Signed-off-by: rraman <rr@chargebee.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='chargebee',
-    version='1.0.5',
+    version='1.0.6',
     author='ChargeBee',
     author_email='support@chargebee.com',
     url='https://apidocs.chargebee.com/docs/api?lang=python',
