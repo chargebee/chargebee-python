de708ae8525070c012f5c25f82f809ec12ce0e5d
sangeetha <sangeetha@chargebee.com>
2016-05-24 09:58:23 +0000

    ** APIs updated**:
    New attribute "currency_code" is returned as part of Credit Note resource.
    See: https://apidocs.chargebee.com/docs/api/credit_notes#credit_note_attributes
    
    The new Gateway type "wepay" for card resource is added. See subscription attributes
    See : https://apidocs.chargebee.com/docs/api/cards#card_attributes

--- chargebee/models/credit_note.py
+++ chargebee/models/credit_note.py
@@ -25,8 +25,8 @@ class CreditNote(Model):
 
     fields = ["id", "customer_id", "subscription_id", "reference_invoice_id", "type", "reason_code", \
     "status", "vat_number", "date", "price_type", "total", "amount_allocated", "amount_refunded", \
-    "amount_available", "refunded_at", "sub_total", "line_items", "discounts", "taxes", "line_item_taxes", \
-    "linked_refunds", "allocations"]
+    "amount_available", "refunded_at", "sub_total", "currency_code", "line_items", "discounts", \
+    "taxes", "line_item_taxes", "linked_refunds", "allocations"]
 
 
     @staticmethod
