377faf2004391edf700f0e94acf5b9a67feacb3f
cb-goutham <goutham@chargebee.com>
2019-08-27 07:02:30 +0000

    New input parameter payment_intent has been added in create_a_gift, create_an_invoice, reactivate_a_subscription and resume_a_subscription api. * New attribute client_profile_id has been added in customer resource. * New input parameter client_profile_id has been added in create_a_customer, update_a_customer, create_subscription_estimate, create_a_subscription and import_a_subscription api. * New input parameter replace_primary_payment_source has been added in create_an_invoice api. * New attribute currency_code has been added in payment_intent resource.

--- chargebee/models/customer.py
+++ chargebee/models/customer.py
@@ -31,7 +31,8 @@ class Customer(Model):
     "primary_payment_source_id", "backup_payment_source_id", "billing_address", "referral_urls", \
     "contacts", "payment_method", "invoice_notes", "preferred_currency_code", "promotional_credits", \
     "unbilled_charges", "refundable_credits", "excess_payments", "balances", "meta_data", "deleted", \
-    "registered_for_gst", "business_customer_without_vat_number", "customer_type", "relationship"]
+    "registered_for_gst", "business_customer_without_vat_number", "customer_type", "client_profile_id", \
+    "relationship"]
 
 
     @staticmethod
--- chargebee/version.py
+++ chargebee/version.py
@@ -1,1 +1,1 @@
-VERSION = '2.6.8'
+VERSION = '2.6.9'
