98769f9fe85b7e31e1cdf5f4deb04ea15ad51b49
cb-alish <162097444+cb-alish@users.noreply.github.com>
2025-10-28 14:45:15 +0530

    Releasing v2.58.0 (#102)

--- chargebee/models/quote_line_group.py
+++ chargebee/models/quote_line_group.py
@@ -14,7 +14,7 @@ class QuoteLineGroup(Model):
       fields = ["line_item_id", "tax_name", "tax_rate", "date_to", "date_from", "prorated_taxable_amount", "is_partial_tax_applied", "is_non_compliance_tax", "taxable_amount", "tax_amount", "tax_juris_type", "tax_juris_name", "tax_juris_code", "tax_amount_in_local_currency", "local_currency_code"]
       pass
     class Discount(Model):
-      fields = ["amount", "description", "entity_type", "discount_type", "entity_id", "coupon_set_code"]
+      fields = ["amount", "description", "line_item_id", "entity_type", "discount_type", "entity_id", "coupon_set_code"]
       pass
     class Tax(Model):
       fields = ["name", "amount", "description"]
